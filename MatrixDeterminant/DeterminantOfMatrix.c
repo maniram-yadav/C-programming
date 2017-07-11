b1283c6fdc5a6ebf1d0e45021b4173a0296f2fff
Maniram Yadav <maniramyadav786@gmail.com>
2017-07-11 18:30:19 +0530

    Update DeterminantOfMatrix.c

--- MatrixDeterminant/DeterminantOfMatrix.c
+++ MatrixDeterminant/DeterminantOfMatrix.c
@@ -1,8 +1,9 @@
 #include<stdio.h>
 
 int main(){
-int a[3][3],i,j,determinant=0,temp=0;
+int a[3][3],i,j,temp=0;
 
+  // Insert element of matrix
 for(i=0;i<3;i++)
 for(j=0;j<3;j++){
 scanf("%d",&a[i][j]);
@@ -12,9 +13,10 @@ scanf("%d",&a[i][j]);
 for(i=0;i<3;i++)
 {
 temp +=(i%2==0?1:-1)*a[i][0]*((i%2==0?1:-1)*a[(i+1)%3][1]*a[(i+2)%3][2]   +(i%2==0?-1:1)*a[(i+2)%3][1]*a[(i+1)%3][2]);
-printf("Det: %d",temp);
 }
-printf("Det: %d\n",temp);
+  
+  // Determinant of the matrix
+printf("Determinant Of Matrix : %d\n",temp);
 
 return 0;
 
